01d88839d9b776754e6a9b3db10ce7c45f5c0c75
Luka Boissiere <luka.boissiere@epitech.eu>
2024-02-05 10:13:01 +0100

    [+] IComponent

--- IComponent.hpp
+++ IComponent.hpp
@@ -1,15 +1,34 @@
 /*
 ** EPITECH PROJECT, 2024
-** NanoTekSpice
+** NTS_BS
 ** File description:
 ** IComponent
 */
 
-#pragma once
+#ifndef ICOMPONENT_HPP_
+    #define ICOMPONENT_HPP_
 
-class IComponent {
-    public:
+#include <iostream>
 
-    protected:
-    private:
-};
+namespace nts
+{
+    enum Tristate {
+        Undefined = ( - true ) ,
+        True = true ,
+        False = false
+    };
+
+    class IComponent
+    {
+        public:
+            virtual ~IComponent() = default;
+            virtual nts::Tristate compute(std::size_t pin = 1) = 0;
+            virtual void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin) = 0;
+            virtual void dump() const = 0;
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !ICOMPONENT_HPP_ */
